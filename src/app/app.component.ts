8338c862015801fc1db1f23fc772d216441315ff
hantute <ahinfohistopro@gmail.com>
2020-05-15 18:03:48 +0200

    add labs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'training-app';
+  //title = 'training-app'; // il s'agit du titre de l'application
+  // session_name = "Formation Web Avancée"; // On définit une variable globale
+  nbpart: number = 0;
+  // on crée une classe firstSession qu'on va exporter dans les classes filles et sur le code html
+  firstSession = {
+    id: 1,
+    name: 'Formation Web Avancée',
+    track: 'MEAN Stack',
+    date: new Date('2020-05-14'),
+    duree: 3,
+    local: 'Lyon',
+    participants: 0
+  };
+
+  nbrParticipantsChange(event) {
+    this.nbpart = event.value;
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AdminModule } from './app/admin/admin.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)  // déclenche le chargement et l'execution de module de type typescript
+  //platformBrowserDynamic().bootstrapModule(AdminModule)  // déclenche le chargement et l'execution de module de type typescript
   .catch(err => console.error(err));
